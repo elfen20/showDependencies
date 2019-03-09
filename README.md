a7f19cca2c0f0e213f1e9eeb064049edf34eea45
Gernot Lenkner <g.lenkner@outlook.de>
2019-03-09 19:08:57 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # showDependencies
 project to show dependencies on vs project files
+
+Usage:
+
+showDependencies.exe <path> <filter>
+
+Path: directory to search csproj files for
+Filter: string to filter dependencies
---
+++ showDependencies/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+
+namespace showDependencies
+{
+    class Program
+    {
+        Dictionary<string, List<string>> dependecies = new Dictionary<string, List<string>>();
+
+        Dictionary<string, List<string>> flatDependecies = new Dictionary<string, List<string>>();
+
+        static void Main(string[] args)
+        {
+            new Program().Run(args);
+        }
+
+        private static void Msg(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        private void Run(string[] args)
+        {
+            string path = Directory.GetCurrentDirectory();
+            string filter = string.Empty;
+
+            if (args.Length > 0)
+            {
+                path = args[0];
+            }
+            if (args.Length > 1)
+            {
+                filter = args[1];
+            }
+
+            string[] projectFiles = Directory.GetFiles(path, "*.csproj", SearchOption.AllDirectories);
+
+            Console.WriteLine($"{projectFiles.Length} Projects found. Analyzing...");
+
+            foreach (string p in projectFiles)
+            {
+                Analyze(p);
+            }
+
+            GenerateFlatDependencies(filter);
+            PrintFlatDependecies();
+        }
+
+        private void GenerateFlatDependencies(string include = "")
+        {
+            foreach (var item in dependecies)
+            {
+                List<string> flatDeps = DependenyArray(item.Key).Where(x => x.Contains(include)).ToList();
+                flatDeps.Sort();
+                flatDependecies.Add(item.Key, flatDeps);
+            }
+        }
+
+        private List<string> DependenyArray(string key)
+        {
+            List<string> result = new List<string>();
+            if (dependecies.ContainsKey(key))
+            {
+                foreach (string dep in dependecies[key])
+                {
+                    result.Add(dep);
+                    result = result.Union(DependenyArray(dep), StringComparer.InvariantCultureIgnoreCase).ToList();
+                }
+            }
+            return result;
+        }
+
+
+        private void PrintFlatDependecies()
+        {
+            foreach (var item in flatDependecies.OrderBy(x => x.Value.Count).ThenBy(n => n.Key))
+            {
+                Console.WriteLine($"{item.Key} : {item.Value.Count}");
+                foreach (string dep in item.Value)
+                {
+                    Console.WriteLine($"\t{dep}");
+                }
+                Console.WriteLine(String.Empty);
+            }
+        }
+
+        private void Analyze(string projectFile)
+        {
+            string pName = Path.GetFileNameWithoutExtension(projectFile);
+            using (XmlReader pReader = XmlReader.Create(projectFile))
+            {
+                while (pReader.Read())
+                {
+                    if (pReader.IsStartElement())
+                    {
+                        switch (pReader.Name.ToLower())
+                        {
+                            case "reference":
+                            case "packagereference":
+                                string include = pReader["Include"];
+                                if (include != null)
+                                {
+                                    AddDependency(pName, include);
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+
+        }
+
+        private void AddDependency(string pName, string include)
+        {
+            List<string> includes;
+            if (!dependecies.ContainsKey(pName))
+            {
+                includes = new List<string>();
+                dependecies.Add(pName, includes);
+            }
+            else
+            {
+                includes = dependecies[pName];
+            }
+            if (!includes.Contains(include))
+            {
+                includes.Add(include);
+            }
+        }
+    }
+}
