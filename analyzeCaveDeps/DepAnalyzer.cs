fc1061533ec6d9ca817409dbbe8cd6009c22f6f3
Gernot Lenkner <g.lenkner@outlook.de>
2019-11-19 17:06:53 +0100

    added new WPF Project AnalyzeCaveDeps

---
+++ analyzeCaveDeps/DepAnalyzer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace analyzeCaveDeps
+{
+    public struct CaveProject
+    {
+        public string Name;
+        public string FileName;
+        public string Solution;
+        public string Version;
+    }
+
+    public class DepAnalyzer
+    {
+        public string BasePath { get; private set; }
+        public Dictionary<string, CaveProject> Projects { get; private set; }
+
+        public DepAnalyzer(string directory)
+        {
+            BasePath = Path.Combine(directory);
+            Projects = new Dictionary<string, CaveProject>();
+            Analyze();
+        }
+
+        void Analyze()
+        {
+            FindProjectFiles();
+        }
+
+        void FindProjectFiles()
+        {
+            string[] projectFiles = Directory.GetFiles(BasePath, "*.csproj", SearchOption.AllDirectories);
+            foreach (string project in projectFiles)
+            {
+                CaveProject cProject = FillProjectData(project);
+                Projects.Add(cProject.Name, cProject);
+            }
+        }
+
+        private CaveProject FillProjectData(string projectFile)
+        {
+            CaveProject cProject = new CaveProject { FileName = projectFile };
+            cProject.Solution = Path.GetFileName(Path.GetFullPath(Path.Combine(Path.GetDirectoryName(cProject.FileName), "..")));
+            cProject.Name = cProject.Solution + "/" + Path.GetFileNameWithoutExtension(cProject.FileName);
+            return cProject;
+        }
+    }
+}
---
+++ analyzeCaveDeps/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace analyzeCaveDeps
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DepAnalyzer depAnalyzer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BAnalyze_Click(object sender, RoutedEventArgs e)
+        {
+            depAnalyzer = new DepAnalyzer(TBaseDir.Text);
+            FillLBProjects();
+        }
+
+        private void FillLBProjects()
+        {
+            LBProjects.Items.Clear();
+            foreach (var project in depAnalyzer.Projects.Keys)
+            {
+                LBProjects.Items.Add(project);
+            }            
+        }
+    }
+}
